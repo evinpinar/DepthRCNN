fe6569087f4934015d90bc6b9d6bbf76e233183c
epinar <evin.oernek@tum.de>
2019-10-16 09:57:09 +0200

    roi global model

--- models/model_maskdepthrcnn.py
+++ models/model_maskdepthrcnn.py
@@ -797,6 +797,290 @@ def detection_target_depth2(proposals, gt_class_ids, gt_boxes, gt_masks, gt_dept
     return rois, roi_gt_class_ids, deltas, masks, depths, normals
 
 
+# For only one data sample
+def detection_target_onesample(proposals, gt_class_ids, gt_boxes, gt_masks, gt_depths, gt_normals, config):
+    """ Subsamples proposals and generates target box refinment, class_ids,
+        and masks for each.
+
+        Inputs:
+        proposals: [batch, N, (y1, x1, y2, x2)] in normalized coordinates. Might
+                   be zero padded if there are not enough proposals.
+        gt_class_ids: [batch, MAX_GT_INSTANCES] Integer class IDs.
+        gt_boxes: [batch, MAX_GT_INSTANCES, (y1, x1, y2, x2)] in normalized
+                  coordinates.
+        gt_masks: [batch, MAX_GT_INSTANCES, height, width] of boolean type
+        gt_depths: [batch, height, width] of float32 type
+        gt_normals: [batch, height, width, C=3] of float32 type
+
+        Returns: Target ROIs and corresponding class IDs, bounding box shifts,
+        masks and depths.
+        rois: [batch, TRAIN_ROIS_PER_IMAGE, (y1, x1, y2, x2)] in normalized
+              coordinates
+        target_class_ids: [batch, TRAIN_ROIS_PER_IMAGE]. Integer class IDs.
+        target_deltas: [batch, TRAIN_ROIS_PER_IMAGE, NUM_CLASSES,
+                        (dy, dx, log(dh), log(dw), class_id)]
+                       Class-specific bbox refinments.
+        target_mask: [batch, TRAIN_ROIS_PER_IMAGE, height, width)
+                     Masks cropped to bbox boundaries and resized to neural
+                     network output size.
+        """
+
+
+    print("normals: ", gt_normals.shape)
+    print("mask:", gt_masks.shape)
+    print("depth:", gt_depths.shape)
+    #gt_masked_depths = compute_gt_depths(gt_depths, gt_masks)
+
+    no_crowd_bool = Variable(torch.ByteTensor(proposals.size()[0] * [True]), requires_grad=False)
+    if config.GPU_COUNT:
+        gt_masked_depths = gt_depths.cuda()
+        no_crowd_bool = no_crowd_bool.cuda()
+
+    ## Compute overlaps matrix [proposals, gt_boxes]
+    overlaps = bbox_overlaps(proposals, gt_boxes)
+
+    ## Determine postive and negative ROIs
+    roi_iou_max = torch.max(overlaps, dim=1)[0]
+
+    ## 1. Positive ROIs are those with >= 0.5 IoU with a GT box
+    positive_roi_bool = roi_iou_max >= 0.5
+    # print('positive count', positive_roi_bool.sum())
+
+    ## Subsample ROIs. Aim for 33% positive
+    ## Positive ROIs
+    if positive_roi_bool.sum() > 0:
+        positive_indices = torch.nonzero(positive_roi_bool)[:, 0]
+
+        positive_count = int(config.TRAIN_ROIS_PER_IMAGE *
+                             config.ROI_POSITIVE_RATIO)
+        rand_idx = torch.randperm(positive_indices.size()[0])
+        rand_idx = rand_idx[:positive_count]
+        if config.GPU_COUNT:
+            rand_idx = rand_idx.cuda()
+        positive_indices = positive_indices[rand_idx]
+        positive_count = positive_indices.size()[0]
+        positive_rois = proposals[positive_indices.data, :]
+
+        ## Assign positive ROIs to GT boxes.
+        positive_overlaps = overlaps[positive_indices.data, :]
+        roi_gt_box_assignment = torch.max(positive_overlaps, dim=1)[1]
+        roi_gt_boxes = gt_boxes[roi_gt_box_assignment.data, :]
+        roi_gt_class_ids = gt_class_ids[roi_gt_box_assignment.data]
+
+        ## Compute bbox refinement for positive ROIs
+        deltas = Variable(utils.box_refinement(positive_rois.data, roi_gt_boxes.data), requires_grad=False)
+        std_dev = Variable(torch.from_numpy(config.BBOX_STD_DEV).float(), requires_grad=False)
+        if config.GPU_COUNT:
+            std_dev = std_dev.cuda()
+        deltas /= std_dev
+
+        ## Assign positive ROIs to GT masks
+        roi_masks = gt_masks[roi_gt_box_assignment.data]
+        roi_depths = gt_depths[roi_gt_box_assignment.data]
+        if config.PREDICT_NORMAL:
+            roi_normals = gt_normals[roi_gt_box_assignment.data]
+        # print(" roi normals: ", roi_normals.shape)
+        # print(" roi masks: ", roi_masks.shape)
+
+        ## Compute mask targets
+        boxes = positive_rois
+        if config.USE_MINI_MASK:
+            ## Transform ROI corrdinates from normalized image space
+            ## to normalized mini-mask space.
+            y1, x1, y2, x2 = positive_rois.chunk(4, dim=1)
+            gt_y1, gt_x1, gt_y2, gt_x2 = roi_gt_boxes.chunk(4, dim=1)
+            gt_h = gt_y2 - gt_y1
+            gt_w = gt_x2 - gt_x1
+            y1 = (y1 - gt_y1) / gt_h
+            x1 = (x1 - gt_x1) / gt_w
+            y2 = (y2 - gt_y1) / gt_h
+            x2 = (x2 - gt_x1) / gt_w
+            boxes = torch.cat([y1, x1, y2, x2], dim=1)
+        box_ids = Variable(torch.arange(roi_masks.size()[0]), requires_grad=False).int()
+        if config.GPU_COUNT:
+            box_ids = box_ids.cuda()
+
+        masks = Variable(
+            CropAndResizeFunction(config.MASK_SHAPE[0], config.MASK_SHAPE[1], 0)(roi_masks.unsqueeze(1), boxes,
+                                                                                 box_ids).data,
+            requires_grad=False).squeeze(1)
+        masks = torch.round(masks)
+
+        depths = Variable(
+            CropAndResizeFunction(config.MASK_SHAPE[0], config.MASK_SHAPE[1], 0)(roi_depths.unsqueeze(1), boxes,
+                                                                                 box_ids).data,
+            requires_grad=False).squeeze(1)
+
+        if config.PREDICT_NORMAL:
+            normals = Variable(
+                CropAndResizeFunction(config.MASK_SHAPE[0], config.MASK_SHAPE[1], 0)(roi_normals, boxes,
+                                                                                     box_ids).data,
+                requires_grad=False)
+
+        # print("after crop mask and normals ", masks.shape, normals.shape)
+    ## Threshold mask pixels at 0.5 to have GT masks be 0 or 1 to use with
+    ## binary cross entropy loss.
+    else:
+        positive_count = 0
+
+    ## 2. Negative ROIs are those with < 0.5 with every GT box. Skip crowds.
+    negative_roi_bool = roi_iou_max < 0.5
+    negative_roi_bool = negative_roi_bool & no_crowd_bool
+    ## Negative ROIs. Add enough to maintain positive:negative ratio.
+    if (negative_roi_bool > 0).sum() > 0 and positive_count > 0:
+        negative_indices = torch.nonzero(negative_roi_bool)[:, 0]
+        r = 1.0 / config.ROI_POSITIVE_RATIO
+        negative_count = int(r * positive_count - positive_count)
+        rand_idx = torch.randperm(negative_indices.size()[0])
+        rand_idx = rand_idx[:negative_count]
+        if config.GPU_COUNT:
+            rand_idx = rand_idx.cuda()
+        negative_indices = negative_indices[rand_idx]
+        negative_count = negative_indices.size()[0]
+        negative_rois = proposals[negative_indices.data, :]
+    else:
+        negative_count = 0
+
+    # print('count', positive_count, negative_count)
+    # print(roi_gt_class_ids)
+
+    ## Append negative ROIs and pad bbox deltas and masks that
+    ## are not used for negative ROIs with zeros.
+    if positive_count > 0 and negative_count > 0:
+        rois = torch.cat((positive_rois, negative_rois), dim=0)
+        zeros = Variable(torch.zeros(negative_count), requires_grad=False).int()
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+        roi_gt_class_ids = torch.cat([roi_gt_class_ids, zeros], dim=0)
+        zeros = Variable(torch.zeros(negative_count, 4), requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+        deltas = torch.cat([deltas, zeros], dim=0)
+        zeros = Variable(torch.zeros(negative_count, config.MASK_SHAPE[0], config.MASK_SHAPE[1]),
+                         requires_grad=False)
+        zeros_normal = Variable(torch.zeros(negative_count, 3, config.MASK_SHAPE[0], config.MASK_SHAPE[1]),
+                                requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+            zeros_normal = zeros_normal.cuda()
+
+        # print("masks, normals, zeros before: ", masks.shape, normals.shape, zeros.shape)
+        masks = torch.cat([masks, zeros], dim=0)
+        depths = torch.cat([depths, zeros], dim=0)
+        # print("then masks: ", masks.shape)
+        if config.PREDICT_NORMAL:
+            normals = torch.cat([normals, zeros_normal], dim=0)
+
+        zeros = Variable(torch.zeros(negative_count, config.NUM_PARAMETERS), requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+    elif positive_count > 0:
+        rois = positive_rois
+    elif negative_count > 0:
+        rois = negative_rois
+        zeros = Variable(torch.zeros(negative_count), requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+        roi_gt_class_ids = zeros
+        zeros = Variable(torch.zeros(negative_count, 4), requires_grad=False).int()
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+        deltas = zeros
+        zeros = Variable(torch.zeros(negative_count, config.MASK_SHAPE[0], config.MASK_SHAPE[1]), requires_grad=False)
+        zeros_normal = Variable(torch.zeros(negative_count, 3, config.MASK_SHAPE[0], config.MASK_SHAPE[1]),
+                                requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+            zeros_normal = zeros_normal.cuda()
+        masks = zeros
+        depths = zeros
+        normals = zeros_normal
+
+        zeros = Variable(torch.zeros(negative_count, config.NUM_PARAMETERS), requires_grad=False)
+        if config.GPU_COUNT:
+            zeros = zeros.cuda()
+    else:
+        rois = Variable(torch.FloatTensor(), requires_grad=False)
+        roi_gt_class_ids = Variable(torch.IntTensor(), requires_grad=False)
+        deltas = Variable(torch.FloatTensor(), requires_grad=False)
+        masks = Variable(torch.FloatTensor(), requires_grad=False)
+        depths = Variable(torch.FloatTensor(), requires_grad=False)
+        normals = Variable(torch.FloatTensor(), requires_grad=False)
+        if config.GPU_COUNT:
+            rois = rois.cuda()
+            roi_gt_class_ids = roi_gt_class_ids.cuda()
+            deltas = deltas.cuda()
+            masks = masks.cuda()
+            depths = depths.cuda()
+            normals = normals.cuda()
+            pass
+
+    if not config.PREDICT_NORMAL:
+        normals = Variable(torch.FloatTensor(), requires_grad=False)
+        normals = normals.cuda()
+
+    return rois, roi_gt_class_ids, deltas, masks, depths, normals
+
+
+def detection_target_batch(proposals, gt_class_ids, gt_boxes, gt_masks, gt_depths, gt_normals, config):
+
+    rois = []
+    has_rcnn_predictions = True
+    if proposals is None:
+        has_rcnn_predictions = False
+
+    if has_rcnn_predictions:
+        roi_bbox_deltas = []
+
+    target_class_ids = []
+    target_deltas = []
+    target_mask = []
+
+    n_proposals_per_sample= 100 # ??
+    n_gts_per_sample= 10
+
+    print("   =>>> detection target batch!! ")
+    print("proposals: ", proposals.shape, " gt boxes: ", gt_boxes.shape)
+
+    for sample_i, (n_props, n_gts) in enumerate(zip(n_proposals_per_sample, n_gts_per_sample)):
+        sample_roi_class_logits = None
+        if n_props > 0 and n_gts > 0:
+            if has_rcnn_predictions:
+                sample_prop_bbox_deltas = proposals[sample_i, :n_props]
+            else:
+                sample_prop_bbox_deltas = None
+            sample_rois, sample_roi_gt_class_ids, sample_deltas, sample_masks, sample_depths, \
+                sample_normals = detection_target_onesample(sample_prop_bbox_deltas, gt_class_ids[sample_i, :n_gts], proposals[sample_i, :n_gts], gt_masks[sample_i, :n_gts], gt_depths[sample_i, :n_gts], gt_normals[sample_i, :n_gts], config)
+            print(" one sample, sample_rois: ", sample_rois.shape, " deltas: ", sample_deltas.shape)
+            if len(sample_rois):
+                sample_rois = sample_rois.unsqueeze(0)
+                if has_rcnn_predictions:
+                    sample_roi_bbox_deltas = sample_roi_bbox_deltas.unsqueeze(0)
+                sample_roi_gt_class_ids = sample_roi_gt_class_ids.unsqueeze(0)
+                sample_deltas = sample_deltas.unsqueeze(0)
+                sample_masks = sample_masks.unsqueeze(0)
+        else:
+            sample_rois = proposals.data.new()
+            if has_rcnn_predictions:
+                sample_roi_bbox_deltas = proposals.data.new()
+            sample_roi_gt_class_ids = gt_class_ids.data.new()
+            sample_deltas = proposals.data.new()
+            sample_masks = gt_masks.data.new()
+        rois.append(sample_rois)
+        if has_rcnn_predictions:
+            roi_bbox_deltas.append(sample_roi_bbox_deltas)
+        target_class_ids.append(sample_roi_gt_class_ids)
+        target_deltas.append(sample_deltas)
+        target_mask.append(sample_masks)
+
+    n_dets_per_sample = 5
+
+    # rois, roi_gt_class_ids, deltas, masks, depths, normals
+    # rois, target_class_ids, target_deltas, target_mask, target_depth, target_normals
+    return rois,roi_gt_class_ids, target_deltas, target_mask, target_depth, target_normals, n_dets_per_sample
+
+
+
 def data_generator2(dataset, config, shuffle=True, augment=False, augmentation=None,
                    random_rois=0, batch_size=1, detection_targets=False,
                    no_augmentation_sources=None):
@@ -2199,7 +2483,7 @@ class MaskDepthRCNN(nn.Module):
             ## Note that proposal class IDs, gt_boxes, and gt_masks are zero
             ## padded. Equally, returned rois and targets are zero padded.
             rois, target_class_ids, target_deltas, target_mask, target_depth, target_normals = \
-                detection_target_depth(rpn_rois, gt_class_ids, gt_boxes, gt_masks, gt_depths, gt_normals, self.config)
+                detection_target_batch(rpn_rois, gt_class_ids, gt_boxes, gt_masks, gt_depths, gt_normals, self.config)
 
 
           #  print(" gt boxes shape: ", gt_boxes.shape)
@@ -2386,6 +2670,8 @@ class MaskDepthRCNN(nn.Module):
             ## Subsamples proposals and generates target outputs for training
             ## Note that proposal class IDs, gt_boxes, and gt_masks are zero
             ## padded. Equally, returned rois and targets are zero padded.
+            print("create detection layers rpns: ", rpn_rois.shape, "class ids:", gt_class_ids.shape,
+                  "gt boxes", gt_boxes.shape, "gt depths", gt_depths.shape)
             rois, target_class_ids, target_deltas, target_mask, target_depth, target_normals = \
                 detection_target_depth2(rpn_rois, gt_class_ids, gt_boxes, gt_masks, gt_depths, gt_normals, self.config)
 
